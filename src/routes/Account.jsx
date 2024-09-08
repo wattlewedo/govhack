3eed5a33fc4d7453e7ab4bb704ccfb426f6eec3c
jonjoshelvey0 <oliver.stachowiak@student.adelaide.edu.au>
2024-09-08 12:46:52 +0930

    kadsjfh

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,11 +1,40 @@
 import "./account.css";
 
 const Account = () => {
-  return (
-    <div className="account">
-      <h1>Account</h1>
+    return (
+    // <div className="account">
+    //   <h1>Account</h1>
+    // </div>
+
+    <div className="container">
+    <h1>Create Account</h1>
+    <form className="form">
+    <input type="text" placeholder="Username" className="input-field" />
+    <input type="email" placeholder="Email" className="input-field" />
+    <input type="password" placeholder="Password" className="input-field" />
+    <input type="password" placeholder="Confirm Password" className="input-field" />
+    <button type="button" className="btn">NEXT</button>
+    </form>
+
+    <div className="divider">
+    <hr className="line" />
+    <span>Join With</span>
+    <hr className="line" />
     </div>
-  );
+
+    <div className="social-login">
+    <button className="social-button">
+        <img src="google-icon.png" alt="Google" className="social-icon"/>
+        <p>Google</p>
+    </button>
+    </div>
+
+    <div className="footer">
+    <p>Already Have an Account?</p>
+    <button className="login-btn">Login</button>
+    </div>
+    </div>
+    );
 };
 
 export default Account;
